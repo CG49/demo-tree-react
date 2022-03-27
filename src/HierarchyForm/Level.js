61258a1bf744e0a34ac4c6710249eefec5c4888a
Chaitanya Ghule <38359593+CG49@users.noreply.github.com>
2022-03-27 20:43:49 +0530

    Update Level.js

--- src/HierarchyForm/Level.js
+++ src/HierarchyForm/Level.js
@@ -1,13 +1,14 @@
 import React, { useRef } from 'react'
 
 // External Libraries
-import { Form, Button } from 'react-bootstrap'
 import { components } from 'react-select'
 import AsyncSelect from 'react-select/async'
+import { Form, Button } from 'react-bootstrap'
+import { DashCircleFill, Search } from 'react-bootstrap-icons'
 
 // css
 import './scss/Level.scss'
-
+console.log( DashCircleFill )
 const defaultStyle = {
   addButton: {
     width: '550px',
@@ -20,19 +21,15 @@ const defaultStyle = {
   },
 }
 
-const SearchIcon = props => {
-  const image = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30" width="20px" height="20px" aria-hidden="true" focusable="false"><path d="M 13 3 C 7.4889971 3 3 7.4889971 3 13 C 3 18.511003 7.4889971 23 13 23 C 15.396508 23 17.597385 22.148986 19.322266 20.736328 L 25.292969 26.707031 A 1.0001 1.0001 0 1 0 26.707031 25.292969 L 20.736328 19.322266 C 22.148986 17.597385 23 15.396508 23 13 C 23 7.4889971 18.511003 3 13 3 z M 13 5 C 17.430123 5 21 8.5698774 21 13 C 21 17.430123 17.430123 21 13 21 C 8.5698774 21 5 17.430123 5 13 C 5 8.5698774 8.5698774 5 13 5 z"/></svg>'
-
-  return (
-    <components.DropdownIndicator { ...props }>
-      <img src={ `data:image/svg+xml;utf8,${ encodeURIComponent( image ) }` } alt="" />
-    </components.DropdownIndicator>
-  )
-}
+const CustomSearchIcon = props => (
+  <components.DropdownIndicator { ...props }>
+    <Search color='black' />
+  </components.DropdownIndicator>
+)
 
 // NOTE: Refs are not being used; refs were added for the purpose of validation
 // TODO: We can remove refs if validation of fields not necessary
-export const Level = React.forwardRef( ( { options, handleCollapsible, isRootLevel = false }, ref ) => {
+export const Level = React.forwardRef( ( { options, handleCollapsible, isExpanded, isRootLevel = false }, ref ) => {
   const {
     isAdd,
     rootOid,
@@ -97,7 +94,7 @@ export const Level = React.forwardRef( ( { options, handleCollapsible, isRootLev
         onClick={ () => isRootLevel && onClickCollapsibleHandler() }
         className={ `icon-container${ ( !isRootLevel || isAdd ) ? ' icon-hidden' : '' }` }
       >
-        <span className='icon caret-down'></span>
+        <span className={ `caret caret-${ isExpanded ? 'up' : 'down' }` }></span>
       </div>
 
       { isAdd ?
@@ -119,7 +116,7 @@ export const Level = React.forwardRef( ( { options, handleCollapsible, isRootLev
                 loadOptions={ promiseOptions }
                 defaultOptions={ globalLevelData }
                 placeholder={ `Select ${ placeholder }` }
-                components={ { DropdownIndicator: SearchIcon } }
+                components={ { DropdownIndicator: CustomSearchIcon } }
                 onChange={ value => onChangeHandler( 'CHANGE', value ) }
                 styles={ {
                   container: provided => ( {
@@ -135,6 +132,7 @@ export const Level = React.forwardRef( ( { options, handleCollapsible, isRootLev
               bsPrefix='remove-button'
               onClick={ () => onClickHandler( 'REMOVE' ) }
             >
+              <DashCircleFill size={ 15 } />
               <span>Remove { buttonText }</span>
             </Button>
           </>
